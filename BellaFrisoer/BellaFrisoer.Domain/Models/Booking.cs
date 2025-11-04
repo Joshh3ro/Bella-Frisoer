11da8f908cb595916463509f9d49525c449c4d55
h66yden <h66yden@gmail.com>
2025-11-04 17:36:10 +0100

    Kalender v0.001

---
+++ BellaFrisoer/BellaFrisoer.Domain/Models/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellaFrisoer.Domain.Models
+{
+    public class Booking
+    {
+        public DateTime BookingDateTime { get; set; }
+        public string Customer { get; set; }
+
+        public Booking(DateTime bookingDateTime, string customer)
+        {
+            BookingDateTime = bookingDateTime;
+            Customer = customer;
+
+        }
+    }
+}
