e2c6431d17afea46509e6cb598314e907a06d678
Joshua D.C <35842209+Joshh3ro@users.noreply.github.com>
2025-11-14 15:49:00 +0100

    Update Customer.cs

--- BellaFrisoer/BellaFrisoer.Domain/Models/Customer.cs
+++ BellaFrisoer/BellaFrisoer.Domain/Models/Customer.cs
@@ -1,4 +1,4 @@
-﻿namespace BellaFrisoer.Domain.Models;
+namespace BellaFrisoer.Domain.Models;
 using System.ComponentModel.DataAnnotations;
 
 public class Customer
@@ -9,15 +9,20 @@ public class Customer
     public string? Name { get; set; }
     public ICollection<Booking> Bookings { get; set; } = new List<Booking>(); public string? Email { get; set; }
     public string? Address { get; set; }
-    public string? PhoneNumber { get; set; }
+    public long? PhoneNumber { get; set; }
     public DateTime? BirthDate { get; set; }
 
-    public Customer( string name, string? email, string? address, string? phoneNumber, DateTime? birthDate )
+    public Customer( string name, string? email, string? address, int? phoneNumber, DateTime? birthDate )
     {
         Name = name;
         Email = email;
         Address = address;
         PhoneNumber = phoneNumber;
         BirthDate = birthDate;
+    }
+
+    public Customer()
+    {
+        
     }
 }
