390c795d7a66e9681e0f605a4d79c7de7c89aa89
Aleksanderbd <aleksanderbdemant@gmail.com>
2025-11-26 18:30:11 +0100

    BookingConflict Fixed

--- BellaFrisoer/BellaFrisoer.Application/BookingConflictChecker.cs
+++ BellaFrisoer/BellaFrisoer.Application/BookingConflictChecker.cs
@@ -6,16 +6,8 @@ using BellaFrisoer.Domain.Models;
 
 namespace BellaFrisoer.Application
 {
-    // TODO: BookingConflict virker ikke på forskellige startdatoer, fx når de overlapper men starter på forskellige dage og bookingen går over til næste dag.
     public class BookingConflictChecker : IBookingConflictChecker
     {
-        public bool HasDateTimeConflict(DateTime newStartTime, TimeSpan newDuration, DateTime existingStartTime, TimeSpan existingDuration)
-        {
-            DateTime newEndTime = newStartTime.Add(newDuration);
-            DateTime existingEndTime = existingStartTime.Add(existingDuration);
-            return newStartTime < existingEndTime && existingStartTime < newEndTime;
-        }
-
         public bool HasBookingConflict(Booking newBooking, IEnumerable<Booking> existingBookings)
         {
             foreach (var booking in existingBookings)
@@ -27,16 +19,20 @@ namespace BellaFrisoer.Application
 
                 // Normalize both start times to the booking date + time-of-day to avoid mismatches in Date component.
                 var newStart = newBooking.BookingDate.Date + newBooking.BookingStartTime.TimeOfDay;
+                var newEnd = newBooking.BookingEndTime;
+
                 var existingStart = booking.BookingDate.Date + booking.BookingStartTime.TimeOfDay;
+                var existingEnd = booking.BookingEndTime;
 
                 // Be robust: ignore bookings missing a valid duration
                 if (newBooking.BookingDuration <= TimeSpan.Zero || booking.BookingDuration <= TimeSpan.Zero)
                     continue;
 
-                if (HasDateTimeConflict(newStart, newBooking.BookingDuration, existingStart, booking.BookingDuration))
+                if (newStart < existingEnd && existingStart < newEnd)
                 {
-                    return true;
+                    return true; // There is a conflict
                 }
+
             }
             return false;
         }
