e557de5054e7f110bcb8e2a719dbf83d9466135c
KasBen4711 <kasper_bennetsen@hotmail.com>
2025-12-01 14:12:10 +0100

    Edit
    Der kan nu redigeres i Bookings og kunde detaljer og Medarbejder detaljer

--- BellaFrisoer/BellaFrisoer.Domain/Models/Customer.cs
+++ BellaFrisoer/BellaFrisoer.Domain/Models/Customer.cs
@@ -1,17 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using BellaFrisoer.Domain.Interfaces;
 using BellaFrisoer.Domain.Models;
 
-public class Customer : IPerson
+namespace BellaFrisoer.Domain.Models
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public class Customer : IPerson
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
-    public string Name { get; set; } = string.Empty;
-    public ICollection<Booking>? Bookings { get; set; } = new List<Booking>();
-    public long? PhoneNumber { get; set; }
+        // single valid Name property with a default
+        public string Name { get; set; } = string.Empty;
 
-    public Customer() { }
+        // single initialized collection
+        public ICollection<Booking>? Bookings { get; set; } = new List<Booking>();
+
+        public long? PhoneNumber { get; set; }
+
+        public Customer() { }
+    }
 }
