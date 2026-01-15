bdaec1e0679198f77e91af2cdd7054cae1a999fc
h66yden <h66yden@gmail.com>
2026-01-15 23:25:42 +0100

    smallFixes

--- BellaFrisoer/BellaFrisoer.Domain/Models/Discounts/BronzeDiscount.cs
+++ BellaFrisoer/BellaFrisoer.Domain/Models/Discounts/BronzeDiscount.cs
@@ -4,13 +4,10 @@ public class BronzeDiscount : IDiscountStrategy
 {
     private const decimal Rate = 0.05m;
 
-
-
     public BronzeDiscount(){}
-
 
     public decimal Apply(Booking booking, Customer customer)
     { 
-        return amount - (amount * Rate); 
+        return booking.BasePrice - (booking.BasePrice * Rate); 
     }
 }
