4d4ebfd39e288ed9afa36b6198706623bf89f107
Joshua D.C <35842209+Joshh3ro@users.noreply.github.com>
2026-01-20 12:32:17 +0100

    Updated booking delete

--- BellaFrisoer/BellaFrisoer.Application/DTOs/BookingDeleteDto.cs
+++ BellaFrisoer/BellaFrisoer.Application/DTOs/BookingDeleteDto.cs
@@ -2,12 +2,7 @@ namespace BellaFrisoer.Application.DTOs;
 /// <summary>
 ///  Opretter vores delete objekt til booking command
 /// </summary>
-public class BookingDeleteDto
+public record BookingDeleteDto
 {
     public int Id { get; init; }
-    public int CustomerId { get; init; }
-    public int EmployeeId { get; init; }
-    public int TreatmentId { get; init; }
-    public DateTime BookingDate { get; init; }
-    public TimeOnly BookingStartTime { get; init; }
 }
